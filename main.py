c6ed40dad7f95c8ed2dbeb3c4fdcec9bcedc9165
Masud <yymasud@gmail.com>
2024-08-05 14:28:30 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,22 @@
+from question_model import Question
+from data import question_data
+from quiz_brain import QuizBrain
+
+question_bank = []
+score= 0
+
+for question in question_data:
+    question_text = question["question"]
+    question_answer = question["correct_answer"]
+    new_question = Question(question_text, question_answer)
+    question_bank.append(new_question)
+
+quiz = QuizBrain(question_bank)
+
+
+while quiz.still_has_question():
+    quiz.next_question()
+
+
+print("You have completed quiz")
+print(f"YOur Final score was: {quiz.score}/{len(question_bank)}")
